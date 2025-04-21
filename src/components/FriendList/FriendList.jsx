5892a60cde9c0b4df0c0efb19d4bdf8d2294ef20
Okan Özdemir <ookanozdemir@gmail.com>
2025-04-22 00:16:24 +0300

    components oluşturuldu.

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const App = () => {
+const FriendList = () => {
   return (
     <div>
-      
+    
     </div>
   )
 }
 
-export default App
+export default FriendList
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const App = () => {
+const Profile = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const App = () => {
   )
 }
 
-export default App
+export default Profile
